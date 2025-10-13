1145a3e03e4e161d33516b2d4dace78ff641af2b
damlaMtn <damla91@gmail.com>
2025-10-13 21:37:46 +0300

    Angular HttpClient, Services and Environment Files

--- AccountOwnerClient/src/app/app-module.ts
+++ AccountOwnerClient/src/app/app-module.ts
@@ -1,6 +1,9 @@
 import { NgModule, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
 import { BrowserModule, provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { CollapseModule } from 'ngx-bootstrap/collapse';
+import { HttpClientModule } from '@angular/common/http';
+
+
 
 import { AppRoutingModule } from './app-routing-module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -19,6 +22,7 @@ import { NotFound } from './error-pages/not-found/not-found';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
+    HttpClientModule,
     CollapseModule.forRoot(),
     NotFound
   ],
